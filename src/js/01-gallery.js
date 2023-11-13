9b5398e436cd223d65d3c2e1e71b2b0f3c957284
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2023-11-13 21:11:48 +0200

    done1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+
+const contaiter = document.querySelector(".gallery");
+const markup = galleryItems.map(
+  ({ preview, description, original }) =>
+    `<li class="gallery__item"><a class="gallery__link"  href="${original}"><img class="gallery__image" src="${preview}"  alt="${description}"></a></li> `
+);
+contaiter.insertAdjacentHTML("beforeend", markup.join(" "));
+
+
+    const gallery= new SimpleLightbox(".gallery__link" , {
+        captionsData: "alt",
+        captionPosition:  "bottom",
+        captionDelay : 250
+    });
+
