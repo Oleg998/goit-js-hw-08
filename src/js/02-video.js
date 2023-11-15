c65b797a2ccc697fbd10e2df06204a0e789d37d7
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2023-11-15 20:49:08 +0200

    done3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player  from '@vimeo/player';
+const iframe = document.querySelector('#vimeo-player');
+
+//  player = new Player(iframe);
+
+// const getTime = player.on('play', onPlay);
+
+// function onPlay(evt) {
+//     player.getCurrentTime().then(evt.seconds)}
+
+//  console.log(getTime);  
+
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
 
+const KEY_FORM = 'feedback-form-state';
+dataSave();
+const formData = {};
+
+form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', onSubmit);
+
+function dataSave() {
+  const saveData = JSON.parse(localStorage.getItem(KEY_FORM));
+  if (saveData) {
+    form.elements.email.value = saveData.email;
+    form.elements.message.value = saveData.message;
+  }
+}
+
+function onInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+
+  localStorage.setItem(KEY_FORM, JSON.stringify(formData));
+}
+
+function onSubmit(evt) {
+  evt.preventDefault();
+
+  const email = evt.target.email.value;
+  const message = evt.target.message.value;
+
+  if (!email || !message) {
+    alert('Please fill in both email and message fields before submitting.');
+    return;
+  }
+  evt.target.reset();
+  localStorage.removeItem(KEY_FORM);
+}
