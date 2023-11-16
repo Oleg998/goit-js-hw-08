4f8cac8d1a1dda7fca6c2b79dd905113c014b601
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2023-11-16 15:20:44 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,21 @@
 import Player  from '@vimeo/player';
+import throttle from 'lodash.throttle';
 const iframe = document.querySelector('#vimeo-player');
+const KEY_TIME = "videoplayer-current-time"
 
-//  player = new Player(iframe);
+player = new Player(iframe);
+dataSave();
+function dataSave() {
+    const saveData = JSON.parse(localStorage.getItem(KEY_TIME));
+    if (saveData) {
+        player.setCurrentTime(saveData)
+    }
+}
+player.on('timeupdate', throttle(timeData,1000));
 
-// const getTime = player.on('play', onPlay);
 
-// function onPlay(evt) {
-//     player.getCurrentTime().then(evt.seconds)}
+function timeData(evt) {
+    const timeNow= evt.seconds ; 
+    localStorage.setItem(KEY_TIME, JSON.stringify(timeNow));
 
-//  console.log(getTime);  
-
-
-
+}
