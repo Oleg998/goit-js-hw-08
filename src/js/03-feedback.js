fc0451be7af4b08bc4aa4221470067ae2ceff92d
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2023-11-17 14:05:45 +0200

    done_3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,8 +11,8 @@ form.addEventListener('submit', onSubmit);
 function dataSave() {
   const saveData = JSON.parse(localStorage.getItem(KEY_FORM));
   if (saveData) {
-    form.elements.email.value = saveData.email;
-    form.elements.message.value = saveData.message;
+    form.elements.email.value = saveData.email ?? "";
+    form.elements.message.value = saveData.message ?? "";
   }
 }
 
@@ -26,12 +26,13 @@ function onSubmit(evt) {
   evt.preventDefault();
 
   const email = evt.target.email.value;
-  const message = evt.target.message.value;
+  const message = evt.target.message.value.trim();
 
   if (!email || !message) {
     alert('Please fill in both email and message fields before submitting.');
     return;
   }
+  console.log(formData);
   evt.target.reset();
   localStorage.removeItem(KEY_FORM);
 }
